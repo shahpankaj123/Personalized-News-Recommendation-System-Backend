43038a2edad4f011aaa7918194f73ae5b9677c1a
Aaryan Shah <aaryanshah615@gmail.com>
2024-05-18 21:20:34 +0545

    db connect

--- project2/settings.py
+++ project2/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'project2.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'news_db',
+        'USER': 'root',
+        'PASSWORD': 'Pankaj123@',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
